4f75ef658a7a41a6ddb4827bf956caedc16c85df
Jordan Miguel <email@jordanmiguel.com>
2026-05-02 20:41:38 -0300

    refactor: drop WuzDeviceMessage and download_media surface
    Model + migration + relationship deleted; download_media config removed.
    Consumers persist messages and download media via event listeners.

--- config/wuz.php
+++ config/wuz.php
@@ -4,7 +4,6 @@ return [
     'enabled' => env('WUZ_ENABLED', false),
     'api_url' => env('WUZ_API_URL', 'http://localhost:8080'),
     'admin_token' => env('WUZ_ADMIN_TOKEN'),
-    'download_media' => env('WUZ_DOWNLOAD_MEDIA', false),
 
     'webhook' => [
         'path' => 'api/wuz/webhook/{token}',
@@ -30,7 +29,6 @@ return [
 
     'table_names' => [
         'devices' => 'wuz_devices',
-        'device_messages' => 'wuz_device_messages',
         'callback_logs' => 'wuz_callback_logs',
         'device_webhooks' => 'wuz_device_webhooks',
         'phone_jids' => 'wuz_phone_jids',
